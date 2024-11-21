1a254260ce92e9bf607ddc0b815b53fd6d60d491
chendehai <dehai.chen@liteng.io>
2024-11-21 10:55:23 +0800

    fix:fix jrpc2.0 params logic

--- rpc/client.go
+++ rpc/client.go
@@ -550,8 +550,14 @@ func (c *Client) newMessage(method string, paramsIn ...interface{}) (*jsonrpcMes
 	msg := &jsonrpcMessage{Version: vsn, ID: c.nextID(), Method: method}
 	if paramsIn != nil { // prevent sending "params":null
 		var err error
-		if msg.Params, err = json.Marshal(paramsIn); err != nil {
-			return nil, err
+		if len(paramsIn) == 1 {
+			if msg.Params, err = json.Marshal(paramsIn[0]); err != nil {
+				return nil, err
+			}
+		} else {
+			if msg.Params, err = json.Marshal(paramsIn); err != nil {
+				return nil, err
+			}
 		}
 	}
 	return msg, nil
